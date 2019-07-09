06c1cbb60d2ba803c4a76757d9a6ff5e6ef3faa1
rahul <rahul.jain1@tothenew.com>
2019-07-10 01:26:42 +0530

    buzzFeed and buzzThread stateless component made

--- client/src/components/Buzz/BuzzFeeds/BuzzFeeds.js
+++ client/src/components/Buzz/BuzzFeeds/BuzzFeeds.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import { connect } from 'react-redux'
 // import { getBuzz, clearBuzz } from '../../../action/buzz.action'
 import BuzzThreads from '../BuzzThreads/BuzzThreads';
@@ -7,7 +7,7 @@ import './BuzzFeedStyle.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilter } from '@fortawesome/free-solid-svg-icons'
 
-class BuzzFeeds extends Component {
+const BuzzFeeds = (props) => {
     // constructor(props) {
     //     super(props);
     //     this.state = {
@@ -39,48 +39,48 @@ class BuzzFeeds extends Component {
     //     }, 500);
     // }
 
-    render() {
-        const filterbtn = <FontAwesomeIcon icon={faFilter} />
-        const { emailId } = this.props.loggedInUser;
-        const userFeeds = this.props.userFeeds;
-        const skip = this.props.skip;
-        return (
-            <div>
-                <div className='filtercategory' onChange={()=>this.props.handleOnChange(event)}>
-                    <span className='filtericon'>{filterbtn}</span>
-                    <select id='buzz-filter' className="buzzFilter" name="filter">
-                        <option value="Most Recent">Most Recent Buzz</option>
-                        <option value="Activity">Activity Buzz</option>
-                        <option value="Lost and Found">Lost and Found Buzz</option>
-                        <option value="My Buzz">My Buzz</option>
-                    </select>
-                </div>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={()=>{this.props.loadFunc()}}
-                    hasMore={skip < userFeeds.length}
-                    loader={<div className="loader" key={0}>Loading ...</div>}
-                >
-                    <div>
-                        {userFeeds.map((data, index) => {
-                            return (
-                                <BuzzThreads 
-                                    feeds={data} 
-                                    loginUser={emailId} 
-                                    key={index} 
-                                    postDelete={this.props.postDelete}
-                                    postLike={this.props.postLike}
-                                    postDislike={this.props.postDislike} 
-                                />
-                            )
-                        })}
-                    </div>
-                </InfiniteScroll>
-
+    const filterbtn = <FontAwesomeIcon icon={faFilter} />
+    const { emailId } = props.loggedInUser;
+    const userFeeds = props.userFeeds;
+    const skip = props.skip;
+    
+    return (
+        <div>
+            <div className='filtercategory' onChange={() => props.handleOnChange(event)}>
+                <span className='filtericon'>{filterbtn}</span>
+                <select id='buzz-filter' className="buzzFilter" name="filter">
+                    <option value="Most Recent">Most Recent Buzz</option>
+                    <option value="Activity">Activity Buzz</option>
+                    <option value="Lost and Found">Lost and Found Buzz</option>
+                    <option value="My Buzz">My Buzz</option>
+                </select>
             </div>
-        )
-    }
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={() => { props.loadFunc() }}
+                hasMore={skip < userFeeds.length}
+                loader={<div className="loader" key={0}>Loading ...</div>}
+            >
+                <div>
+                    {userFeeds.map((data, index) => {
+                        return (
+                            <BuzzThreads
+                                feeds={data}
+                                loginUser={emailId}
+                                key={index}
+                                postDelete={props.postDelete}
+                                postLike={props.postLike}
+                                postDislike={props.postDislike}
+                            />
+                        )
+                    })}
+                </div>
+            </InfiniteScroll>
+
+        </div>
+    )
 }
+
 
 // const mapStateToProps = (state) => {
 //     return {
--- client/src/components/Buzz/BuzzThreads/BuzzThreads.js
+++ client/src/components/Buzz/BuzzThreads/BuzzThreads.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import { connect } from 'react-redux'
 // import { postLike, postDislike, postDelete } from '../../../action/buzz.action'
 import moment from 'moment';
@@ -6,85 +6,84 @@ import './buzzthreadStyle.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 import { deleteAlert } from '../../../utils/actionAlert'
-class BuzzThreads extends Component {
 
-    like = () => {
-        const buzzId = this.props.feeds._id;
-        this.props.postLike(buzzId);
+const BuzzThreads = (props) => {
+
+    const likeFn = () => {
+        const buzzId = props.feeds._id;
+        props.postLike(buzzId);
     }
-    dislike = () => {
-        const buzzId = this.props.feeds._id;
-        this.props.postDislike(buzzId);
+    const dislikeFn = () => {
+        const buzzId = props.feeds._id;
+        props.postDislike(buzzId);
     }
-    onDelete = () => {
-        const buzzId = this.props.feeds._id;
+    const onDelete = () => {
+        const buzzId = props.feeds._id;
 
         let val = deleteAlert();
         val.then(result => {
             if (result.value) {
-                this.props.postDelete(buzzId)
+                props.postDelete(buzzId)
             }
         })
     }
-    checkUserLike = (id, like) => {
+    const checkUserLike = (id, like) => {
 
         const liked = like.find((item) => (item.userId === id));
         return liked ? 'liked' : ''
     }
-    checkUserDislike = (id, dislike) => {
+    const checkUserDislike = (id, dislike) => {
         const disliked = dislike.find((item) => (item.userId === id));
         return disliked ? 'disliked' : ''
     }
 
-    render() {
-        const removeIcon = <FontAwesomeIcon icon={faTimes} />
-        const like = <FontAwesomeIcon icon={faThumbsUp} />
-        const unlike = <FontAwesomeIcon icon={faThumbsDown} />
-        const { loginUser } = this.props
-        const { username, emailId, category, description, attachment, createdAt, Like, dislike, thumbNail } = this.props.feeds;
-        const liked = this.checkUserLike(loginUser, Like);
-        const disliked = this.checkUserDislike(loginUser, dislike);
+    const removeIcon = <FontAwesomeIcon icon={faTimes} />
+    const likeBtn = <FontAwesomeIcon icon={faThumbsUp} />
+    const unlikeBtn = <FontAwesomeIcon icon={faThumbsDown} />
+    const { loginUser } = props
+    const { username, emailId, category, description, attachment, createdAt, Like, dislike, thumbNail } = props.feeds;
+    const liked = checkUserLike(loginUser, Like);
+    const disliked = checkUserDislike(loginUser, dislike);
 
-        return (
-            <div className='buzzContainer'>
-                <div className='buzzHeader'>
-                    <div className='nameActivity'>
-                        <div className='userimgcontainer'>
-                            <img src={thumbNail} alt="" role='presentation' className='userimg' />
-                        </div>
-                        <div className='usernameactivity'>
-                            <span className='username'>{username}</span>
-                            <span className='buzzCategory'>
-                                <span className="badge badge-pill badge-primary">{category}</span>
-                            </span>
-                            <div className='timestamp'>
-                                {moment(createdAt).fromNow()}
-                            </div>
+    return (
+        <div className='buzzContainer'>
+            <div className='buzzHeader'>
+                <div className='nameActivity'>
+                    <div className='userimgcontainer'>
+                        <img src={thumbNail} alt="" role='presentation' className='userimg' />
+                    </div>
+                    <div className='usernameactivity'>
+                        <span className='username'>{username}</span>
+                        <span className='buzzCategory'>
+                            <span className="badge badge-pill badge-primary">{category}</span>
+                        </span>
+                        <div className='timestamp'>
+                            {moment(createdAt).fromNow()}
                         </div>
                     </div>
+                </div>
 
-                    <div className='deleteBuzz'>
-                        {
-                            loginUser === emailId ? <span onClick={this.onDelete} className='deleteicon'>{removeIcon}</span> : null
-                        }
-                    </div>
-                </div>
-                <hr className="headerBorder" />
-                <div className='buzzContentContainer'>
-                    <div className='buzzContent'>
-                        {description}
-                    </div>
-                    <div className='uploadedimg'>
-                        <img src={attachment} alt='' role='presentation' />
-                    </div>
-                </div>
-                <div className='buzzFooter'>
-                    <span className={`likeicon ${liked}`} onClick={this.like}>{like}&nbsp;{Like.length}</span>
-                    <span className={`unlikeicon ${disliked}`} onClick={this.dislike}>{unlike}&nbsp;{dislike.length}</span>
+                <div className='deleteBuzz'>
+                    {
+                        loginUser === emailId ? <span onClick={onDelete} className='deleteicon'>{removeIcon}</span> : null
+                    }
                 </div>
             </div>
-        )
-    }
+            <hr className="headerBorder" />
+            <div className='buzzContentContainer'>
+                <div className='buzzContent'>
+                    {description}
+                </div>
+                <div className='uploadedimg'>
+                    <img src={attachment} alt='' role='presentation' />
+                </div>
+            </div>
+            <div className='buzzFooter'>
+                <span className={`likeicon ${liked}`} onClick={likeFn}>{likeBtn}&nbsp;{Like.length}</span>
+                <span className={`unlikeicon ${disliked}`} onClick={dislikeFn}>{unlikeBtn}&nbsp;{dislike.length}</span>
+            </div>
+        </div>
+    )
 }
 
 
