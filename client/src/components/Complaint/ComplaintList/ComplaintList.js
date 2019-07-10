93d895d83c627001c7b2e92f84a1f76ecd169dc0
rahul <rahul.jain1@tothenew.com>
2019-07-10 14:05:47 +0530

    complainList made stateless component

--- client/src/components/Complaint/ComplaintList/ComplaintList.js
+++ client/src/components/Complaint/ComplaintList/ComplaintList.js
@@ -1,76 +1,77 @@
-import React, { Component } from 'react'
-import { showComplaintList } from '../../../action/complaint.action';
-import { connect } from 'react-redux'
+import React from 'react';
+// import React, { Component } from 'react'
+// import { showComplaintList } from '../../../action/complaint.action';
+// import { connect } from 'react-redux'
 import ComplaintThread from '../ComplaintThread/ComplaintThread';
 import './table.css';
-class ComplaintList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            filter: "All Complaints"
-        }
-    }
-    componentDidMount() {
-        this.props.showComplaintList();
-    }
 
-    handleOnChange = (event) => {
-        this.setState({
-            filter: event.target.value
-        })
-    }
+const ComplaintList = (props) => {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         filter: "All Complaints"
+    //     }
+    // }
+    // componentDidMount() {
+    //     this.props.showComplaintList();
+    // }
 
-    render() {
-        return (
-            <div className="table-wrapper">
-                <header className="table-header">
-                    <span className="left">
-                        {this.state.filter}
-                    </span>
-                    <span className="right">
-                        <select name="filter" onChange={this.handleOnChange}>
-                            <option value="All Complaints">All Complaints</option>
-                            <option value="Pending">Pending</option>
-                            <option value="In Progress">In Progress</option>
-                            <option value="Resolved">Resolved</option>
-                        </select>
-                    </span>
-                </header>
-                    <table className="table table-hover">
-                        <thead className="thead-dark">
-                            <tr>
-                                <th scope="col">Department</th>
-                                <th scope="col">Issue Id</th>
-                                <th scope="col">Assigned To</th>
-                                <th scope="col">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.list.map((data, index) => {
-                                if (this.state.filter === "All Complaints") {
-                                    return (
-                                        <ComplaintThread list={data} key={index} />
-                                    )
-                                } else if (this.state.filter === data.status) {
-                                    return (
-                                        <ComplaintThread list={data} key={index} />
-                                    )
-                                }
-                            })}
-                        </tbody>
-                    </table>
-            </div>
+    // handleOnChange = (event) => {
+    //     this.setState({
+    //         filter: event.target.value
+    //     })
+    // }
 
-        )
-    }
+    return (
+        <div className="table-wrapper">
+            <header className="table-header">
+                <span className="left">
+                    {props.filter}
+                </span>
+                <span className="right">
+                    <select name="filter" onChange={() => props.handleOnChange(event)}>
+                        <option value="All Complaints">All Complaints</option>
+                        <option value="Pending">Pending</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Resolved">Resolved</option>
+                    </select>
+                </span>
+            </header>
+            <table className="table table-hover">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Department</th>
+                        <th scope="col">Issue Id</th>
+                        <th scope="col">Assigned To</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.list.map((data, index) => {
+                        if (props.filter === "All Complaints") {
+                            return (
+                                <ComplaintThread list={data} key={index} />
+                            )
+                        } else if (props.filter === data.status) {
+                            return (
+                                <ComplaintThread list={data} key={index} />
+                            )
+                        }
+                    })}
+                </tbody>
+            </table>
+        </div>
+
+    )
 }
 
-const mapStateToProps = (state) => {
-    return { list: state.complaintReducer.complaintList }
-}
+// const mapStateToProps = (state) => {
+//     return { list: state.complaintReducer.complaintList }
+// }
 
-const mapDispatchToProps = {
-    showComplaintList
-}
+// const mapDispatchToProps = {
+//     showComplaintList
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ComplaintList)
+// export default connect(null, mapDispatchToProps)(ComplaintList)
+export default ComplaintList;
--- client/src/components/Complaint/ComplaintManager/ComplaintManager.js
+++ client/src/components/Complaint/ComplaintManager/ComplaintManager.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import ComplaintForm from '../ComplaintForm/ComplaintForm'
 import ComplaintList from '../ComplaintList/ComplaintList';
+import { showComplaintList } from '../../../action/complaint.action'
 
-const ComplaintManager = (props) => {
+class ComplaintManager extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: "All Complaints"
+        }
+    }
+
+    componentDidMount() {
+        this.props.showComplaintList();
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            filter: event.target.value
+        })
+    }
+
+    render() {
         return (
             <div>
                 <ComplaintForm />
-                <ComplaintList />
+                <ComplaintList
+                    list={this.props.list}
+                    handleOnChange={this.handleOnChange}
+                    filter={this.state.filter}
+                />
             </div>
         )
+    }
+}
+const mapStateToProps = (state) => {
+    return { list: state.complaintReducer.complaintList }
 }
 
-export default ComplaintManager
+const mapDispatchToProps = {
+    showComplaintList
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ComplaintManager)
