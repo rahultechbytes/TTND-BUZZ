136920a3bc813ac9e6fde91b702154773c4abbfa
rahul <rahul.jain1@tothenew.com>
2019-07-22 12:38:14 +0530

    db connection code refactored

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-require('./dB/utils/connection');                          //db connection establishment
+const dataBase = require('./dB/utils/connection');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const PORT = process.env.PORT;
@@ -9,6 +9,7 @@ require('./config/cloudinary');
 const cors = require('cors');
 const routes = require('./routes/routeManager');
 
+dataBase.connect();                                             //db connection establishment
 
 app.use(bodyParser.urlencoded({ extended: false }));            //bodyParser
 app.use(bodyParser.json());
--- server/dB/utils/connection.js
+++ server/dB/utils/connection.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-const dbConfig =require('./config');
-mongoose.connect(dbConfig.dbURL, {useNewUrlParser: true,useFindAndModify:false,useCreateIndex: true});
+const dbConfig = require('./config');
 
-
-const dbConnection = mongoose.connection;
-
-dbConnection.once('open',()=>{
-    console.log('db started..');
-});
+function connect() {
+    mongoose.connect(dbConfig.dbURL, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true });
+    const dbConnection = mongoose.connection;
+    dbConnection.once('open', () => {
+        console.log('db started..');
+    });
+}
 
 // mongoose.set("debug",true);
 
-module.exports = mongoose;
+module.exports = {
+    connect
+}
